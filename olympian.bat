923f0e472d2aded69961035371d495c9a9e20e95
TrueBigPapa <75802529+TrueBigPapa@users.noreply.github.com>
2020-12-10 08:57:32 -0800

    Update olympian.bat

--- olympian.bat
+++ olympian.bat
@@ -4,6 +4,6 @@ cd /d %~dp0
 valueupdater.exe
 START "olympian.exe" olympian.exe
 REM You can change restart time by editing the value below, its in seconds
-timeout /t 3600
+timeout /t 180
 taskkill /f /im "olympian.exe"
 goto loop 
